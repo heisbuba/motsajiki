ca34a99c348c5dba11416aa6ab7f93628c05bfa9
Buba <iabbaabdullahigarba@gmail.com>
2026-08-28 12:19:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker cache version and static asset manifest
-const CACHE_NAME = 'motsa-jiki-v1.4.7'; 
+const CACHE_NAME = 'motsa-jiki-v1.4.8'; 
 const ASSETS = [
   '/',
   '/overview',
